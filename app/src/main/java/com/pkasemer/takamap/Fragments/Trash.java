7fe1b956ed5e14c55809ee20b95055fe0c3e1e86
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-02-28 18:34:08 +0300

    updates

--- app/src/main/java/com/pkasemer/takamap/Fragments/Trash.java
+++ app/src/main/java/com/pkasemer/takamap/Fragments/Trash.java
@@ -36,11 +36,6 @@ import java.util.List;
 public class Trash extends Fragment {
 
 
-    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 101;
-    private ImageView imageView;
-    Button  btnLoadimage;
-    private Bitmap bitmap;
-    Uri imageUri;
 
     public Trash() {
         // Required empty public constructor
@@ -64,168 +59,12 @@ public class Trash extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_trash, container, false);
 
-        imageView = view.findViewById(R.id.my_avatar_imageview);
-        btnLoadimage = view.findViewById(R.id.loadimagesourcebtn);
-        btnLoadimage.setVisibility(View.GONE);
-
-
-        //show uploadimageiconview
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkAndRequestPermissions(getActivity())) {
-                    chooseImage(getActivity());
-                }
-            }
-        });
-
-        btnLoadimage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkAndRequestPermissions(getActivity())) {
-                    chooseImage(getActivity());
-                }
-            }
-        });
 
         return view;
     }
 
 
-    // function to let's the user to choose image from camera or gallery
-    private void chooseImage(Context context) {
-        final CharSequence[] optionsMenu = {"Take Photo", "Choose from Gallery", "Cancel"}; // create a menuOption Array
-        // create a dialog for showing the optionsMenu
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        // set the items in builder
-        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (optionsMenu[i].equals("Take Photo")) {
-                    // Open the camera and get the photo
-                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(takePicture, 0);
-                } else if (optionsMenu[i].equals("Choose from Gallery")) {
-                    // choose from  external storage
-                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(pickPhoto, 1);
-                } else if (optionsMenu[i].equals("Cancel")) {
-                    dialogInterface.dismiss();
-                }
-            }
-        });
-        builder.show();
-    }
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = getActivity().getApplicationContext().getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-
-    // function to check permission
-    public static boolean checkAndRequestPermissions(final Activity context) {
-        int WExtstorePermission = ContextCompat.checkSelfPermission(context,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int cameraPermission = ContextCompat.checkSelfPermission(context,
-                Manifest.permission.CAMERA);
-        List<String> listPermissionsNeeded = new ArrayList<>();
-        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.CAMERA);
-        }
-        if (WExtstorePermission != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded
-                    .add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-        if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(context, listPermissionsNeeded
-                            .toArray(new String[listPermissionsNeeded.size()]),
-                    REQUEST_ID_MULTIPLE_PERMISSIONS);
-            return false;
-        }
-        return true;
-    }
-
-
-    // Handled permission Result
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_ID_MULTIPLE_PERMISSIONS:
-                if (ContextCompat.checkSelfPermission(getActivity(),
-                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(getActivity().getApplicationContext().getApplicationContext(),
-                            "FlagUp Requires Access to Camara.", Toast.LENGTH_SHORT)
-                            .show();
-                } else if (ContextCompat.checkSelfPermission(getActivity(),
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(getActivity().getApplicationContext().getApplicationContext(),
-                            "FlagUp Requires Access to Your Storage.",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    chooseImage(getActivity());
-                }
-                break;
-        }
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != getActivity().RESULT_CANCELED) {
-            switch (requestCode) {
-                case 0:
-                    if (resultCode == getActivity().RESULT_OK && data != null) {
-                        bitmap = (Bitmap) data.getExtras().get("data");
-                        imageView.setVisibility(View.VISIBLE);
-                        imageView.setImageBitmap(bitmap);
-//                        uploadimageiconview.setVisibility(View.GONE);
-                        btnLoadimage.setVisibility(View.VISIBLE);
-                        // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
-                        Uri tempUri = getImageUri(getActivity().getApplicationContext().getApplicationContext(), bitmap);
-                        // CALL THIS METHOD TO GET THE ACTUAL PATH
-                        File finalFile = new File(getRealPathFromURI(tempUri));
-
-
-                    }
-                    break;
-                case 1:
-                    if (resultCode == getActivity().RESULT_OK && data != null) {
-                        Uri selectedImage = data.getData();
-                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
-                        if (selectedImage != null) {
-                            Cursor cursor = getActivity().getApplicationContext().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                            if (cursor != null) {
-                                cursor.moveToFirst();
-                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                                String picturePath = cursor.getString(columnIndex);
-                                bitmap = BitmapFactory.decodeFile(picturePath);
-                                imageView.setVisibility(View.VISIBLE);
-                                imageView.setImageBitmap(bitmap);
-//                                uploadimageiconview.setVisibility(View.GONE);
-                                btnLoadimage.setVisibility(View.VISIBLE);
-
-
-                                cursor.close();
-                            }
-                        }
-                    }
-                    break;
-            }
-        }
-    }
 
 
 }
