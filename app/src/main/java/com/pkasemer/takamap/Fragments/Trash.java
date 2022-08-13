785d8e8e3e327161594d99c1ef084096cd86a9f8
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-08-13 15:20:02 +0300

    update on card colors

--- app/src/main/java/com/pkasemer/takamap/Fragments/Trash.java
+++ app/src/main/java/com/pkasemer/takamap/Fragments/Trash.java
@@ -1,70 +1,43 @@
 package com.pkasemer.takamap.Fragments;
 
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.pkasemer.takamap.Pickup;
 import com.pkasemer.takamap.R;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Trash extends Fragment {
-
-
 
     public Trash() {
         // Required empty public constructor
     }
 
-
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
-    }
-
+    MaterialButton request_pickup;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_trash, container, false);
+        request_pickup = view.findViewById(R.id.request_pickup);
+
+        request_pickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Pickup.class);
+                getContext().startActivity(intent);
+            }
+        });
 
 
         return view;
     }
-
-
-
 
 
 }
