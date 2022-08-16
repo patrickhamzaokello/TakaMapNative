94e9d6ec280e0ee33fb0cadfdf9b18fcbb1235d8
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-08-16 18:46:51 +0300

    getting clear image working:

--- app/src/main/java/com/pkasemer/takamap/MakeReport.java
+++ app/src/main/java/com/pkasemer/takamap/MakeReport.java
@@ -9,24 +9,37 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MakeReport extends AppCompatActivity {
 
     Button btnSelectPhoto;
-    ImageView viewImage;
-    private final int CAMERA_REQUEST_CODE = 100;
-    private static final int REQUEST_CODE = 1;
+    ImageView image_captured;
+    String currentPhotoPath;
+
+    private static final int REQUEST_CODE = 412;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,30 +77,95 @@ public class MakeReport extends AppCompatActivity {
             new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    setImage(result.getResultCode(), result.getData());
+//                    setImage(result.getResultCode(), result.getData());
+                    setPic();
                 }
             });
 
     private void initViewPager() {
-        viewImage = findViewById(R.id.viewImage);
+        image_captured = findViewById(R.id.viewImage);
         btnSelectPhoto = findViewById(R.id.btnSelectPhoto);
 
         btnSelectPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                activityResultLauncher.launch(iCamera);
+                dispatchTakePictureIntent();
             }
         });
+
     }
 
-    private void setImage(int resultCode, @Nullable Intent data){
-        if (resultCode == RESULT_OK) {
-                // for camera
-                assert data != null;
-                Bitmap img = (Bitmap) data.getExtras().get("data");
-                viewImage.setImageBitmap(img);
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        getString(R.string.fileprovider),
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                activityResultLauncher.launch(takePictureIntent);
+            }
         }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = image_captured.getWidth();
+        int targetH = image_captured.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        image_captured.setImageBitmap(bitmap);
     }
 
 }
