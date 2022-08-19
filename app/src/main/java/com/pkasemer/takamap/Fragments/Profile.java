b81553ca29624b1c450a2ef1688f94b02fd0034c
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-08-19 16:40:35 +0300

    profile section complete

--- app/src/main/java/com/pkasemer/takamap/Fragments/Profile.java
+++ app/src/main/java/com/pkasemer/takamap/Fragments/Profile.java
@@ -13,9 +13,10 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.card.MaterialCardView;
 import com.pkasemer.takamap.HelperClasses.SharedPrefManager;
 import com.pkasemer.takamap.LoginMaterial;
-import com.pkasemer.takamap.ManageOrders;
 import com.pkasemer.takamap.Models.UserModel;
 import com.pkasemer.takamap.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 
 public class Profile extends Fragment {
@@ -25,23 +26,46 @@ public class Profile extends Fragment {
     }
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+    TextView full_name_text, username, user_details;
 
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        if (!SharedPrefManager.getInstance(getContext()).isLoggedIn()) {
+//            finish();
+            startActivity(new Intent(getContext(), LoginMaterial.class));
+        }
 
+        full_name_text = view.findViewById(R.id.full_name_text);
+        username = view.findViewById(R.id.username);
+        user_details = view.findViewById(R.id.user_details);
 
+        UserModel user = SharedPrefManager.getInstance(getContext()).getUser();
+        full_name_text.setText(user.getFullname());
+        username.setText(user.getUsername());
+        user_details.setText(user.getEmail() + " | " + user.getPhone());
 
+        view.findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                new SweetAlertDialog(getContext(), SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Before you go!")
+                        .setContentText("You will be required to Sign in next time")
+                        .setConfirmText("OK")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.dismissWithAnimation();
+                                SharedPrefManager.getInstance(getContext()).logout();
+                            }
+                        }).show();
+
+            }
+        });
 
         return view;
     }
